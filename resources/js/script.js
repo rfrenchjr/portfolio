07fc551d9f81d02f7fc76703381fc129afde71ae
R French <rogerfrenchjr@gmail.com>
2023-07-31 11:36:02 -0600

    First commit

---
+++ resources/js/script.js
@@ -0,0 +1,7 @@
+function changeImage() {
+    if (document.getElementById("imgClickAndChange").src == "https://i.imgur.com/zChD9oG.jpg"){
+        document.getElementById("imgClickAndChange").src = "https://i.imgur.com/ox0dzd3.jpg";
+    } else {
+        document.getElementById("imgClickAndChange").src = "https://i.imgur.com/zChD9oG.jpg";
+    }
+}
